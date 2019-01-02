e5caf0b0f232c63324d69fc1902e063857078f03
Henry Doan <henrydoan96@gmail.com>
2019-01-02 12:30:35 -0700

    stubbed out

---
+++ app.js
@@ -0,0 +1,50 @@
+  // alert
+
+  // multi line comment
+
+  // single line comment
+
+  // math
+
+  // order of operation
+
+  // strings
+  
+  // concatination
+
+  // = vs == vs ===
+
+  // integers
+
+  // floats
+
+  // booleans
+
+  // arrays
+
+  // function 
+
+  // calling a function
+
+  // function with params
+
+  // reassignment 
+
+  // console log
+
+// conditions
+
+
+  // array pos
+  // index  
+
+  // array with different types
+
+  // array in array
+
+  // size of the array
+
+  // add item onto the array
+
+
+  // remove the last element from the array
